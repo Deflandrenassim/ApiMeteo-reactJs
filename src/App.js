4893e0158147f32342eef8b8b30851fa923d4489
Deflandrenassim <nassim.def@gmail.com>
2022-01-28 17:13:32 +0100

    addd function and update api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "2387b871ee3326872b7e4f4735f6b383",
   url: "https://api.openweathermap.org/data/2.5/"
@@ -6,6 +6,26 @@ const api = {
 
 
 function App() {
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather]= useState('');
+
+    const search = evt => {
+      if(evt.key === "Enter") {
+        fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result =>  {
+          setWeather(result)
+          setQuery('');
+          console.log(result);
+
+      });
+    }
+  }
+
+
+
+
   const dateBuilder = (d) =>{ /* ma const dateBuilder */
     let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août",
     "Septembre", "Octobre", "Novembre", "Décembre"];
@@ -20,17 +40,20 @@ function App() {
   }
   return (
     <div className="app">
-     <div class="container">
-        <div class="container-search">
+     <div className="container">
+        <div className="container-search">
             <input type="text" 
               className="search-box"
               placeholder="Quel temps fait il ?"
+              onChange= {e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={ search }
               />
         </div>
-          <div class="container-info">
-            <div class="date"> {dateBuilder(new Date())} </div>
-            <div class="ville"> New york </div>
-            <div class="temps"> Temps celsius</div>
+          <div className="container-info">
+            <div className="date"> {dateBuilder(new Date())} </div>
+            <div className="ville"> New york </div>
+            <div className="temp"> Temps celsius</div>
           </div>
         
      </div>
