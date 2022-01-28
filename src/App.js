0ff101d2ed788d689f4fbc291cc0f19a67d9f7c6
Deflandrenassim <nassim.def@gmail.com>
2022-01-28 14:43:49 +0100

    add images and keys api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+const api = {
+  key: "2387b871ee3326872b7e4f4735f6b383",
+  url: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 function App() {
   return (
     <div className="App">
       Hello bienvenu
+    
     </div>
+    
   );
 }
 
