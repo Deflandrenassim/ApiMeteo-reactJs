9fd643c96a7ee8d2882f7478729eea03e096214b
Deflandrenassim <nassim.def@gmail.com>
2022-01-29 11:26:24 +0100

    add type of in main

--- src/App.js
+++ src/App.js
@@ -7,59 +7,60 @@ const api = {
 
 function App() {
 
-    const [query, setQuery] = useState('');
-    const [weather, setWeather]= useState('');
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
 
-    const search = evt => {
-      if(evt.key === "Enter") {
-        fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
-        .then(result =>  {
+        .then(result => {
           setWeather(result)
           setQuery('');
           console.log(result);
 
-      });
+        });
     }
   }
 
-
-
-
-  const dateBuilder = (d) =>{ /* ma const dateBuilder */
+  const dateBuilder = (d) => { /* ma const dateBuilder */
     let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août",
-    "Septembre", "Octobre", "Novembre", "Décembre"];
+      "Septembre", "Octobre", "Novembre", "Décembre"];
     let days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 
     let day = days[d.getDay()];
     let date = d.getDate();
-    let month = months[d.getMonth()]; 
+    let month = months[d.getMonth()];
     let year = d.getFullYear();
 
     return `${day}  ${date} ${month} ${year}`
   }
   return (
     <div className="app">
-     <div className="container">
-        <div className="container-search">
-            <input type="text" 
+      <main>
+        <div className="container">
+          <div className="container-search">
+            <input type="text"
               className="search-box"
               placeholder="Quel temps fait il ?"
-              onChange= {e => setQuery(e.target.value)}
+              onChange={e => setQuery(e.target.value)}
               value={query}
-              onKeyPress={ search }
-              />
-        </div>
-          <div className="container-info">
-            <div className="date"> {dateBuilder(new Date())} </div>
-            <div className="ville"> New york </div>
-            <div className="temp"> Temps celsius</div>
+              onKeyPress={search}
+            />
           </div>
-        
-     </div>
-    
+          {(typeof weather.main != "undefined") ? (
+          <div>
+          <div className="container-info">
+              <div className="date"> {dateBuilder(new Date())} </div>
+              <div className="ville"> {weather.name}, {weather.sys.country}</div>
+              <div className="temp"> {Math.Round(weather.main.temp)}</div>
+          </div>
+          </div>
+          ) : ('') }
+          </div>
+      </main>
     </div>
-    
+
   );
 }
 
